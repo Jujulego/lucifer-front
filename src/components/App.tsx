571101ee22aa284777984ba034f2b8d4f476459b
Julien Capellari <julien@capellari.net>
2020-02-11 16:22:20 +0100

    add: material-ui

---
+++ src/components/App.tsx
@@ -0,0 +1,35 @@
+import React, { useMemo } from 'react';
+
+import {
+  CssBaseline,
+  Typography,
+  useMediaQuery
+} from '@material-ui/core';
+import {
+  ThemeProvider,
+  createMuiTheme
+} from '@material-ui/core/styles';
+
+// Component
+const App = () => {
+  // Theme
+  const prefersDark = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = useMemo(
+    () => createMuiTheme({
+      palette: {
+        type: prefersDark ? 'dark' : 'light'
+      }
+    }),
+    [prefersDark]
+  );
+
+  // Render
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Typography>Hello world !</Typography>
+    </ThemeProvider>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import { StylesProvider } from '@material-ui/core';
+
+import App from 'components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+// Application
+ReactDOM.render((
+  <StylesProvider injectFirst>
+    <App />
+  </StylesProvider>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

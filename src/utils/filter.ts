52b00cbdca4deea08638ace9943e009f11357158
Julien Capellari <julien@capellari.net>
2020-02-18 23:36:03 +0100

    update: filter add array cases

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,11 +1,34 @@
 // Types
-export type Filter<T> = Partial<T>;
+type FilterValue<T> = T extends Array<infer P> ? P | T : T | Array<T>;
+
+export type Filter<T> = { [K in keyof T]?: FilterValue<T[K]> };
 export type Predicate<T> = (obj: T) => boolean;
 
 // Utils
+function test<T>(value: T, filter?: FilterValue<T>): boolean {
+  if (!filter) return true;
+
+  // value & filter are arrays: all filters must be in value
+  if (Array.isArray(filter) && Array.isArray(value)) {
+    return filter.every(v => value.indexOf(v) !== -1);
+  }
+
+  // filter is array: value must be in filter
+  if (Array.isArray(filter)) {
+    return filter.indexOf(value) !== -1;
+  }
+
+  // value is array: filter must be in value
+  if (Array.isArray(value)) {
+    return value.indexOf(filter) !== -1;
+  }
+
+  return value === filter;
+}
+
 export function toPredicate<T>(filter: Filter<T>): Predicate<T> {
   const fields = Object.keys(filter) as Array<keyof T>;
-  return (obj) => fields.every(field => obj[field] === filter[field]);
+  return (obj) => fields.every(field => test(obj[field], filter[field]));
 }
 
 export function filter<T>(array: T[], filter: Filter<T>): T[] {

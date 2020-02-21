5a383f2aa2b5c440aa6fcb4656f0a0e6f0fbc9b4
Julien Capellari <julien@capellari.net>
2020-02-21 12:16:18 +0100

    update: CopyFab's icon fade to check icon on copy success

--- src/components/basics/CopyFab.tsx
+++ src/components/basics/CopyFab.tsx
@@ -1,25 +1,66 @@
-import React, { MouseEvent } from 'react';
+import React, { MouseEvent, useState, useEffect } from 'react';
 import copy from 'copy-to-clipboard';
+import clsx from 'clsx';
+import { pick } from 'lodash';
 
-import { Fab, FabProps } from '@material-ui/core';
+import {
+  Fade,
+  Fab, FabProps, FabClassKey
+} from '@material-ui/core';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import CheckIcon from '@material-ui/icons/Check';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 
+import { StyledProps } from 'utils/style';
+
 // Types
-export interface CopyFabProps extends FabProps {
+export type CopyFabClassKey = FabClassKey | 'succeed';
+export type CopyFabProps = Omit<FabProps, 'classes'> & StyledProps<CopyFabClassKey> & {
   text: string;
   format?: string;
 
   onCopied?: () => void;
 }
 
+// Styles
+const useStyles = makeStyles(({ palette }: Theme) => ({
+  label: {
+    display: 'grid',
+    justifyItems: 'center',
+
+    '& > *': {
+      gridArea: '1 / 1 / 2 / 2',
+    }
+  },
+  succeed: {
+    backgroundColor: palette.success.main,
+
+    '&:hover': {
+      backgroundColor: palette.success.dark,
+    }
+  }
+}));
+
 // Component
 const CopyFab = (props: CopyFabProps) => {
   // Props
   const {
-    text, format = "text/plain",
+    text, classes,
+    format = "text/plain",
     onCopied, onClick,
     ...btn
   } = props;
+
+  // State
+  const [succeed, setSucceed] = useState(false);
+
+  // Effects
+  useEffect(() => {
+    if (succeed) {
+      const id = setTimeout(() => setSucceed(false), 2000);
+      return () => clearTimeout(id);
+    }
+  }, [succeed, setSucceed]);
 
   // Handler
   const handleCopy = (event: MouseEvent<HTMLButtonElement>) => {
@@ -27,13 +68,30 @@ const CopyFab = (props: CopyFabProps) => {
 
     // Copy
     const success = copy(text, { format });
-    if (success && onCopied) onCopied();
+    if (success) {
+      setSucceed(true);
+      if (onCopied) onCopied();
+    }
   };
 
   // Render
+  const styles = useStyles({ classes: pick(classes, ['succeed', 'label']) });
+
   return (
-    <Fab {...btn} onClick={handleCopy}>
-      <FileCopyIcon fontSize="small" />
+    <Fab
+      {...btn} onClick={handleCopy}
+      classes={{
+        ...classes,
+        root: clsx(classes?.root, { [styles.succeed]: succeed }),
+        label: styles.label,
+      }}
+    >
+      <Fade in={succeed} timeout={250}>
+        <CheckIcon />
+      </Fade>
+      <Fade in={!succeed} timeout={250}>
+        <FileCopyIcon fontSize="small" />
+      </Fade>
     </Fab>
   );
 };

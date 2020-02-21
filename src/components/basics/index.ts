699f351e5c2fe7ec9abbedf7738f0d2d65607dfc
Julien Capellari <julien@capellari.net>
2020-02-21 14:05:51 +0100

    add: ClosableDialogTitle

--- src/components/basics/index.ts
+++ src/components/basics/index.ts
@@ -1,11 +1,14 @@
 export * from './Fields';
 export * from './Table';
 
+export { default as ClosableDialogTitle } from './ClosableDialogTitle';
+export * from './ClosableDialogTitle';
+
 export { default as CopyButton } from './CopyButton';
 export * from './CopyButton';
 
-export { default as CopyFab } from 'components/basics/CopyFab';
-export * from 'components/basics/CopyFab';
+export { default as CopyFab } from './CopyFab';
+export * from './CopyFab';
 
 export { default as RelativeDate } from './RelativeDate';
 export * from './RelativeDate';
--- src/components/tokens/NewTokenDialog.tsx
+++ src/components/tokens/NewTokenDialog.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
 import {
-  IconButton,
-  Dialog, DialogTitle, DialogContent,
+  Dialog, DialogContent,
   Paper,
   Typography, Theme
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import CloseIcon from '@material-ui/icons/Close';
 
 import { background } from 'utils/colors';
 
-import { CopyFab } from 'components/basics';
+import { ClosableDialogTitle, CopyFab } from 'components/basics';
 
 // Types
 export interface NewTokenDialogProps {
@@ -22,17 +20,6 @@ export interface NewTokenDialogProps {
 
 // Styles
 const useStyles = makeStyles(({ palette, spacing }: Theme) => ({
-  title: {
-    display: 'flex',
-
-    '& > h2': {
-      flexGrow: 1,
-    }
-  },
-  closeBtn: {
-    marginTop: -8,
-    marginBottom: -8,
-  },
   paper: {
     marginBottom: spacing(2),
     padding: spacing(1),
@@ -45,7 +32,7 @@ const useStyles = makeStyles(({ palette, spacing }: Theme) => ({
     float: 'right',
     marginLeft: spacing(1),
     marginBottom: spacing(1)
-  }
+  },
 }));
 
 // Component
@@ -64,12 +51,7 @@ const NewTokenDialog = (props: NewTokenDialogProps) => {
       open={open} onClose={() => onClose()}
       maxWidth="xs" fullWidth
     >
-      <DialogTitle disableTypography classes={{ root: styles.title }}>
-        <Typography component="h2" variant="h6">Nouveau token</Typography>
-        <IconButton classes={{ root: styles.closeBtn }} edge="end" onClick={() => onClose()}>
-          <CloseIcon />
-        </IconButton>
-      </DialogTitle>
+      <ClosableDialogTitle onClose={() => onClose()}>Nouveau token</ClosableDialogTitle>
       <DialogContent>
         <Paper elevation={0} classes={{ root: styles.paper }}>
           <CopyFab text={token} classes={{ root: styles.fab }} size="small" color="secondary" />

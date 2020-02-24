b34be6b01f7ce69c43c7996f5c20a63ebb3b0795
Julien Capellari <julien@capellari.net>
2020-02-25 00:15:17 +0100

    update: move LevelLetter component in it's own file

--- src/components/permissions/PermissionLevel.tsx
+++ src/components/permissions/PermissionLevel.tsx
@@ -1,51 +1,27 @@
 import React, { memo } from 'react';
-import clsx from 'clsx';
-import { capitalize } from 'lodash';
 
-import { Typography, Tooltip } from '@material-ui/core';
-import { makeStyles, Theme } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Permission from 'data/permission';
-import { permissionOption, decomposeLevel } from 'utils/permissions';
+import { permissionOption, decomposeLevel, LEVELS } from 'utils/permissions';
+
+import LevelLetter from './levels/LevelLetter';
 
 // Types
-interface LetterProps { active: boolean, value: string, option?: string | null }
-
 export interface PermissionLevelProps {
   permission: Permission
 }
 
 // Styles
-const useStyles = makeStyles(({ palette }: Theme) => ({
+const useStyles = makeStyles({
   root: {
     minWidth: 42,
     textAlign: 'center'
-  },
-  disabled: {
-    color: palette.text.disabled,
-    pointerEvents: 'none'
   }
-}));
-
-// Components
-const Letter = memo((props: LetterProps) => {
-  // Props
-  const { active, value, option } = props;
-
-  // Render
-  const styles = useStyles();
-  const text = capitalize(option || value);
-
-  if (option === null) return null;
-  return (
-    <Tooltip title={text}>
-      <span className={clsx({ [styles.disabled]: !active })}>
-        { text[0] }
-      </span>
-    </Tooltip>
-  );
 });
 
+// Component
 const PermissionLevel = memo((props: PermissionLevelProps) => {
   // Props
   const { permission } = props;
@@ -57,10 +33,9 @@ const PermissionLevel = memo((props: PermissionLevelProps) => {
 
   return (
     <Typography color="secondary" classes={{ root: styles.root }}>
-      <Letter active={lvl.create} value="create" option={opts.level?.create} />
-      <Letter active={lvl.read}   value="read"   option={opts.level?.read}   />
-      <Letter active={lvl.update} value="update" option={opts.level?.update} />
-      <Letter active={lvl.delete} value="delete" option={opts.level?.delete} />
+      { LEVELS.map(name => (
+        <LevelLetter name={name} active={lvl[name]} options={opts} />
+      )) }
     </Typography>
   )
 });

239972467d15219f5513dc424e0463a8257e8317
Julien Capellari <julien@capellari.net>
2020-06-02 11:19:52 +0200

    update: show token (with copy button) on home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
-import { Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 
 import { useAuth } from 'auth/auth.context';
+import { useAuthToken } from 'auth/auth.hooks';
+
+import { CopyButton } from 'basics/components';
 
 // Component
 const Home = () => {
   // Auth
   const { user } = useAuth();
+  const token = useAuthToken();
 
   // Render
   return (
     <>
       <Typography>Bonjour { user?.nickname } !</Typography>
+      <Grid container wrap="nowrap" alignItems="center" spacing={2}>
+        <Grid item xs zeroMinWidth>
+          <Typography noWrap>{ token }</Typography>
+        </Grid>
+        <Grid item xs="auto">
+          <CopyButton text={token} />
+        </Grid>
+      </Grid>
     </>
   );
 };

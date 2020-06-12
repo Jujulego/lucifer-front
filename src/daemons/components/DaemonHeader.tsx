cc11b51cd7e4feb9e20cdb2cab3ed6e083cca28c
Julien Capellari <julien@capellari.net>
2020-06-12 12:16:41 +0200

    update: add link to owner in daemon header

--- src/daemons/components/DaemonHeader.tsx
+++ src/daemons/components/DaemonHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
-import { Grid, IconButton, Typography } from '@material-ui/core';
+import { Grid, IconButton, Link, Typography } from '@material-ui/core';
 import { Refresh as RefreshIcon } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -42,7 +43,13 @@ const DaemonHeader = (props: DaemonHeaderProps) => {
     <Grid className={styles.root} container alignItems="center">
       <Grid item xs zeroMinWidth>
         <Typography variant="h5" noWrap>Daemon { daemon.name || daemon.id }</Typography>
-        <Typography variant="subtitle1" color="textSecondary">{ daemon.owner?.name }</Typography>
+        <Typography variant="subtitle1" color="textSecondary">
+          { daemon.owner && (
+            <Link component={RouterLink} to={`/users/${daemon.owner.id}/daemons`} color="inherit">
+              { daemon.owner.name }
+            </Link>
+          ) }
+        </Typography>
       </Grid>
       <Grid className={styles.action} item xs="auto">
         <IconButton onClick={onReload}>
